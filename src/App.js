c131b2ebbc338fc129ab4a8a78344bd6f06df6c2
xz33 <xz33@illinois.edu>
2019-05-18 13:58:41 -0500

    Set up react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+//import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    //axios.get("https://developer.nps.gov/api/v1/parks?stateCode=il&api_key=d89uHqV2SoNnB9ojknPK9NfumXPRF73cUNEFeibp")
+    var self = this;
+    
+    axios.get("https://developer.nps.gov/api/v1/parks", {
+      params: {
+        stateCode: 'CA',
+        limit: 10,
+        api_key: process.env.REACT_APP_API_KEY
+      }
+    })
+    .then(res => {
+      // use ( process.env.REACT_APP_API_KEY ) to api key private;
+      console.log(res);
+      self.setState({
+        data: res.data.data
+      });
+    });
+    
+  }
+
+  render() {
+    console.log(this.state.data);
+    var p = "";
+    if (this.state.data.length !== 0) {
+      p = this.state.data[0];
+    }
+
+    return (
+      <>
+        <p>Hi! This is the landing page :)</p>
+        <p>{ p.description }</p>
+      </>
+    );
+  }
+
 }
 
 export default App;
+
+
