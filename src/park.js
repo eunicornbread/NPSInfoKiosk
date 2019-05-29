7b9e4c719763de58ba633d15bc5aed39d96d2e3f
xz33 <xz33@illinois.edu>
2019-05-29 01:06:58 -0500

    Convert part of the visitor center page. Broke a lot of stuff and hoepfully did fix all of them

--- src/park.js
+++ src/park.js
@@ -230,7 +230,7 @@ class Park extends Component {
 		          	</div>
 
 
-					<div className='left' id='left'>
+					<div className='left-side' id='left-side'>
 
 					<div className="navigation" onClick={ this.handleClick }>
 						<ul className="nav flex-column" id="pills-tab" role="tablist">
@@ -255,7 +255,7 @@ class Park extends Component {
 
 
 					</div>
-					<div className='right' id='right'>
+					<div className='right-side' id='right-side'>
 
 					
 
--- src/visitorcenter.js
+++ src/visitorcenter.js
@@ -4,6 +4,19 @@ import directionIcon from './svg/directions-black-22.svg';
 import visitorIcon from './svg/visitor-center-black-18.svg';
 
 class VisitorCenter extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	handleClick(index, event) {
+		document.getElementById('visitor-list').classList.add('hide');
+		document.getElementById('visitor-list').classList.remove('show');
+		setTimeout(() => {
+			document.getElementById('camp-' + index).classList.remove('hide');
+			document.getElementById('camp-' + index).classList.add('show');
+		}, 500);
+	}
+
 	render() {
 		console.log(this.props.data);
 		if (this.props.data.length === 0) {
@@ -14,99 +27,92 @@ class VisitorCenter extends Component {
 			);
 		}
 
+		var visitorDetail = [];
 		var visitorList = [];
+
 		this.props.data.forEach((element, index) => {
 			visitorList.push(
-				<div key={ index }>
-					<div className='visitor-item mb-5' data-toggle="modal" data-target={'#visitorModal' + index}>
-						<p>{ element.name }</p>
-						<p>{ element.description }</p>
-					</div>
-
-					<div className="modal fade" id={'visitorModal' + index} tabIndex="-1" role="dialog" 
-						aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-						<div className="modal-dialog modal-dialog-centered modal-lg" role="document">
-							<div className="modal-content">
-							    <div className='close-modal'>
-							        <button type="button" className="close" data-dismiss="modal" 
-							        	aria-label="Close">
-							          <span aria-hidden="true">&times;</span>
-							        </button>
-							    </div>
-
-								<div className="modal-body">
-									<h3>
-										{ element.name }
-										<img src={ visitorIcon } alt='visitor center icon' id='visitor-icon' />
-									</h3>
-								  
-									<p className='info-title'>Description</p>
-									{ element.description === "" && 
-										<p className='info-detail'>None</p>
-									}
-									{ element.description !== "" && 
-										<p className='info-detail'>{ element.description }</p>
-									}
-								  
-									<p className='info-title'>URL</p>
-									{ element.url === "" && 
-										<p className='info-detail'>None</p>
-									}
-									{ element.url !== "" && 
-										<div className='link-url'>
-											<a href={ element.url } target="_blank" rel="noopener noreferrer" className='info-detail'>
-												{ element.url }
-												<span><i className="fas fa-external-link-alt link-icon"></i></span>
-											</a>
-										</div>
-									}
-
-									<h3>
-										Direction
-										<img src={ directionIcon } alt='direction icon' id='direction-icon' />
-									</h3>
-
-									<p className='info-title'>Info</p>
-									{ element.directionsInfo === "" && 
-										<p className='info-detail'>None</p>
-									}
-									{ element.directionsInfo !== "" && 
-										<p className='info-detail'>{ element.directionsInfo }</p>
-									}
-
-									<p className='info-title'>URL</p>
-									{ element.directionsUrl === "" && 
-										<p className='info-detail'>None</p>
-									}
-									{ element.directionsUrl !== "" && 
-										<div className='link-url' id="direction-url">
-											<a href={ element.directionsUrl } target="_blank" rel="noopener noreferrer" className='info-detail'>
-												{ element.directionsUrl }
-												<span><i className="fas fa-external-link-alt link-icon"></i></span>
-											</a>
-										</div>
-									}
-
-									<p className='info-title'>Latitude & Longitude</p>
-									{ element.latLong === "" && 
-										<p className='info-detail'>None</p>
-									}
-									{ element.latLong !== "" && 
-										<p className='info-detail'>{ element.latLong }</p>
-									}
-
-								</div>
-					    	</div>
-						</div>
-					</div>
+				<div className='visitor-item' data-index={index} key={index} onClick={ this.handleClick.bind(this, index) }>
+					<p data-index={index}>{ element.name }</p>
+					<p data-index={index}>{ element.description }</p>
 				</div>
+			);
+
+			
+
+			visitorDetail.push(
+				<div className="detail-page hide" id={'camp-' + index} key={ index }>
+					<h3>
+						{ element.name }
+						<img src={ visitorIcon } alt='visitor center icon' id='visitor-icon' />
+					</h3>
+				  
+					<p className='info-title'>Description</p>
+					{ element.description === "" && 
+						<p className='info-detail'>None</p>
+					}
+					{ element.description !== "" && 
+						<p className='info-detail'>{ element.description }</p>
+					}
+				  
+					<p className='info-title'>URL</p>
+					{ element.url === "" && 
+						<p className='info-detail'>None</p>
+					}
+					{ element.url !== "" && 
+						<div className='link-url'>
+							<a href={ element.url } target="_blank" rel="noopener noreferrer" className='info-detail'>
+								{ element.url }
+								<span><i className="fas fa-external-link-alt link-icon"></i></span>
+							</a>
+						</div>
+					}
+
+					<h3>
+						Direction
+						<img src={ directionIcon } alt='direction icon' id='direction-icon' />
+					</h3>
+
+					<p className='info-title'>Info</p>
+					{ element.directionsInfo === "" && 
+						<p className='info-detail'>None</p>
+					}
+					{ element.directionsInfo !== "" && 
+						<p className='info-detail'>{ element.directionsInfo }</p>
+					}
+
+					<p className='info-title'>URL</p>
+					{ element.directionsUrl === "" && 
+						<p className='info-detail'>None</p>
+					}
+					{ element.directionsUrl !== "" && 
+						<div className='link-url' id="direction-url">
+							<a href={ element.directionsUrl } target="_blank" rel="noopener noreferrer" className='info-detail'>
+								{ element.directionsUrl }
+								<span><i className="fas fa-external-link-alt link-icon"></i></span>
+							</a>
+						</div>
+					}
+
+					<p className='info-title'>Latitude & Longitude</p>
+					{ element.latLong === "" && 
+						<p className='info-detail'>None</p>
+					}
+					{ element.latLong !== "" && 
+						<p className='info-detail'>{ element.latLong }</p>
+					}
+
+				</div>		
 			);
 		});
 		return (
-			<div>
-				
-				<div className='visitor-list'>
+			<div className="visitor-wrapper">
+				<div className='visitor-list show' id='visitor-list'>
 					{ visitorList }
+				</div>
+
+				<div className='visitor-detail' id='visitor-detail'>
+					{ visitorDetail }
 				</div>	
 			</div>
 		);
