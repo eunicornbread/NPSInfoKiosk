781d63775d1d87fdda326f1a9454c871c695657d
xz33 <xz33@illinois.edu>
2019-06-08 12:50:16 -0500

    Change web title

--- src/modal.js
+++ src/modal.js
@@ -56,6 +56,16 @@ class AttributionModal extends Component {
 
 			        <div className='attr-item'>
 			        	<i className="fas fa-circle solid-dot-icon"></i>
+			        	Web Logo
+			        </div>
+			        <div className='attr-item attr-link'>Icons made by <a href="https://www.freepik.com/" 
+			        	title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> 
+			        	is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 
+			        	title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">CC 3.0 BY</a>
+			        </div>
+
+			        <div className='attr-item'>
+			        	<i className="fas fa-circle solid-dot-icon"></i>
 			        	Return Icon
 			        </div>
 			        <div className='attr-item attr-link'>Icons made by <a href="https://www.flaticon.com/authors/google" 
